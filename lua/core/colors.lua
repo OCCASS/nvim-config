8606c5abe515b9389c26fc04bcd929666bb75207
OCCCAS <pavlov.timur556@yandex.ru>
2023-11-17 00:58:31 +0300

    init: initial commit

---
+++ lua/core/colors.lua
@@ -0,0 +1,3 @@
+vim.opt.termguicolors = true
+require("gruvbox").setup({italic = {strings = false}})
+vim.cmd.colorscheme("gruvbox")
---
+++ lua/core/mappings.lua
@@ -0,0 +1,8 @@
+-- Neotree
+vim.keymap.set("n", "<C-t>", ":Neotree toggle<CR>")
+
+-- Nohl
+vim.keymap.set("n", "<C-n>", ":nohl<CR>")
+
+-- Esc
+vim.keymap.set("i", "kj", "<Esc>", {noremap = true})
