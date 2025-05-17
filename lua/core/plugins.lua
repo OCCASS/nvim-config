a684b09765d924a4111cd1bedf35a517386546e8
Тимур Павлов <pavlov.timur556@yandex.ru>
2025-05-17 11:40:55 +0300

    fix(ls): null-ls set to none-ls

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -1,33 +1,64 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
-        "git", "clone", "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git", "--branch=stable", -- latest stable release
-        lazypath
-    })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-    {
-        "nvim-neo-tree/neo-tree.nvim",
-        branch = "v3.x",
-        dependencies = {
-            "nvim-lua/plenary.nvim", "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-            "MunifTanjim/nui.nvim"
-        }
-    }, {"nvim-treesitter/nvim-treesitter"}, {"neovim/nvim-lspconfig"},
-    -- Themes packages
-    {"ellisonleao/gruvbox.nvim", priority = 1000, config = true, opts = ...},
-    {"craftzdog/solarized-osaka.nvim", lazy = true, priority = 1000, opts = {}},
-    -- Themes packages end
-    {"hrsh7th/cmp-nvim-lsp"}, {"hrsh7th/cmp-buffer"}, {"hrsh7th/cmp-path"},
-    {"hrsh7th/cmp-cmdline"}, {"hrsh7th/nvim-cmp"}, {"lewis6991/gitsigns.nvim"},
-    {"williamboman/mason.nvim"}, {
-        "nvim-telescope/telescope.nvim",
-        tag = "0.1.4",
-        dependencies = {"nvim-lua/plenary.nvim"}
-    }, {"jose-elias-alvarez/null-ls.nvim"},
-    {"L3MON4D3/LuaSnip", version = "v2.*", build = "make install_jsregexp"},
-    {"stevearc/conform.nvim", opts = {}},
+	{
+		"nvim-neo-tree/neo-tree.nvim",
+		branch = "v3.x",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-tree/nvim-web-devicons",
+			"MunifTanjim/nui.nvim",
+		},
+	},
+	{ "nvim-treesitter/nvim-treesitter" },
+	{ "neovim/nvim-lspconfig" },
+	-- Themes packages
+	{ "ellisonleao/gruvbox.nvim", priority = 1000, config = true, opts = ... },
+	{ "craftzdog/solarized-osaka.nvim", lazy = true, priority = 1000, opts = {} },
+	-- Themes packages end
+	{ "hrsh7th/cmp-nvim-lsp" },
+	{ "hrsh7th/cmp-buffer" },
+	{ "hrsh7th/cmp-path" },
+	{ "hrsh7th/cmp-cmdline" },
+	{ "hrsh7th/nvim-cmp" },
+	{ "lewis6991/gitsigns.nvim" },
+	{ "williamboman/mason.nvim" },
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.4",
+		dependencies = { "nvim-lua/plenary.nvim" },
+	},
+	{
+		"nvimtools/none-ls.nvim",
+		event = { "BufReadPre", "BufNewFile" },
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"williamboman/mason.nvim",
+		},
+		config = function()
+			require("plugins.nullls")
+		end,
+	},
+	{ "L3MON4D3/LuaSnip", version = "v2.*", build = "make install_jsregexp" },
+	{ "stevearc/conform.nvim", opts = {} },
+	{
+		"iamcco/markdown-preview.nvim",
+		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+		build = "cd app && yarn install",
+		init = function()
+			vim.g.mkdp_filetypes = { "markdown" }
+		end,
+		ft = { "markdown" },
+	},
 })
