bf90769723a3ddd5a8d820134e10f9a9cf7fc215
Тимур Павлов <pavlov.timur556@yandex.ru>
2025-10-08 20:24:46 +0300

    config updated

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -1,8 +1,19 @@
 local conform = require("conform")
-
-conform.setup({formatters_by_ft = {python = {"isort"}}})
+conform.setup({
+	formatters_by_ft = {
+		lua = { "stylua" },
+		python = { "isort", "black", "autoflake", "reorder-python-imports" },
+		rust = { "rust-analyzer" }, -- no lsp_format here
+		javascript = { "prettierd", "prettier" }, -- stop after first available
+	},
+	format_on_save = {
+		lsp_fallback = true,
+	},
+})
 
 vim.api.nvim_create_autocmd("BufWritePre", {
-    pattern = "*",
-    callback = function(args) conform.format({bufnr = args.buf}) end
+	pattern = "*",
+	callback = function(args)
+		conform.format({ bufnr = args.buf })
+	end,
 })
--- lua/plugins/nullls.lua
+++ lua/plugins/nullls.lua
@@ -1,34 +1,37 @@
 local null_ls = require("null-ls")
-local group = vim.api.nvim_create_augroup("lsp_format_on_save", {clear = false})
+local group = vim.api.nvim_create_augroup("lsp_format_on_save", { clear = false })
 
 null_ls.setup({
-    sources = {
-        null_ls.builtins.formatting.eslint_d.with {
-            filetypes = {
-                "typescript", "javascript", "typescriptreact", "javascriptreact"
-            }
-        }, null_ls.builtins.diagnostics.eslint_d,
-        null_ls.builtins.formatting.lua_format,
-        null_ls.builtins.formatting.prettier.with {
-            filetypes = {
-                "css", "scss", "less", "html", "json", "jsonc", "yaml",
-                "markdown", "markdown.mdx", "graphql", "handlebars"
-
-            }
-        }, null_ls.builtins.formatting.black
-    },
-    on_attach = function(client, bufnr)
-        client.server_capabilities.document_formatting = true
-        if client.supports_method("textDocument/formatting") then
-            vim.api.nvim_clear_autocmds({group = group, buffer = bufnr})
-            vim.api.nvim_create_autocmd("BufWritePre", {
-                group = group,
-                buffer = bufnr,
-                callback = function()
-                    vim.lsp.buf.format({bufnr = bufnr, async = false})
-                    -- vim.lsp.buf.formatting_sync()
-                end
-            })
-        end
-    end
+	sources = {
+		null_ls.builtins.formatting.stylua,
+		null_ls.builtins.formatting.prettier.with({
+			filetypes = {
+				"css",
+				"scss",
+				"less",
+				"html",
+				"json",
+				"jsonc",
+				"yaml",
+				"markdown",
+				"markdown.mdx",
+				"graphql",
+				"handlebars",
+				"astro",
+			},
+		}),
+		null_ls.builtins.formatting.black,
+	},
+	on_attach = function(client, bufnr)
+		if client.supports_method("textDocument/formatting") then
+			vim.api.nvim_clear_autocmds({ group = group, buffer = bufnr })
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				group = group,
+				buffer = bufnr,
+				callback = function()
+					vim.lsp.buf.format({ bufnr = bufnr, async = false })
+				end,
+			})
+		end
+	end,
 })
