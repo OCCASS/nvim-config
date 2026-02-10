a0d1b3ba9a335bdf8ab71fcfc4af17f270ebbc50
Тимур Павлов <pavlov.timur556@yandex.ru>
2026-02-10 10:25:59 +0300

    config updated

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -6,3 +6,7 @@ vim.keymap.set("n", "<C-n>", ":nohl<CR>")
 
 -- Esc (kj to <esc> mapping disabled)
 -- vim.keymap.set("i", "kj", "<Esc>", {noremap = true})
+
+-- Fix reseting select when move to tab
+vim.keymap.set("v", ">", ">gv", { noremap = true, silent = true })
+vim.keymap.set("v", "<", "<gv", { noremap = true, silent = true })
